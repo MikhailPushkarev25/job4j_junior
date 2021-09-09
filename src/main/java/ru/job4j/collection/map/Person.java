2c1fade007c1100882a724496dffa23cc0c4acb9
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-09-09 22:56:38 +0300

    9.1. Рассказать и продемонстрировать как переопределяют метод hashCode [#9881 #14999]

---
+++ src/main/java/ru/job4j/collection/map/Person.java
@@ -0,0 +1,58 @@
+package ru.job4j.collection.map;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+    /*
+    Работа метода hashcode and equals происходит следующим образом,
+    при запуске программы сначала запускается метод hashcode он переводит обьект в индекс
+    для определения ячейки в массиве, и так далее проверяет что нет ли такого индекса в таблице
+    далее передает управление методу equals он поверяет обьект на идентичность.
+     */
+}

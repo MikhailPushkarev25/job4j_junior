af7b8fd9ab2e5e7b916bf2db805c7eb1ac2f15db
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-09-14 22:26:41 +0300

    2. Анализ доступности сервера. [#859 #15007]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -10,40 +10,41 @@ import java.util.List;
 public class Analizy {
 
     public void unavailable(String source, String target) {
-        boolean b = true;
-        List<String> targets = new ArrayList<>();
-        List<String> sources = new ArrayList<>();
-       try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-           read.lines().forEach(sources::add);
-           for (String str : sources) {
-               if ((str.startsWith("200")) && !b) {
-                   String[] line = str.split(" ");
-                   targets.add(line[1]);
-                   b = true;
-               }
-               if ((str.startsWith("400") || str.startsWith("500")) && b) {
-                   String[] line = str.split(" ");
-                   targets.add(line[1]);
-                   b = false;
-               }
-           }
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-        int i = 0;
-       try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-           for (String str : targets) {
-               out.write(str);
-               i++;
-               if (i % 2 == 0) {
-                   out.write(System.lineSeparator());
-               } else {
-                   out.write(" ");
-               }
-           }
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
+        boolean flag = true;
+      List<String> targets = new ArrayList<>();
+      List<String> sources = new ArrayList<>();
+      try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+          read.lines().forEach(sources::add);
+          for (String str : sources) {
+              if (str.startsWith("200") && !flag) {
+                  String[] line = str.split(" ");
+                  targets.add(line[1]);
+                  flag = true;
+              }
+
+              if ((str.startsWith("400") || str.startsWith("500")) && flag) {
+                  String[] line = str.split(" ");
+                  targets.add(line[1]);
+                  flag = false;
+              }
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      int count = 0;
+      try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+          for (String str : targets) {
+              out.write(str);
+              count++;
+              if (count % 2 == 0) {
+                  out.write(System.lineSeparator());
+              } else {
+                  out.write(" ");
+              }
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
     }
 
     public static void main(String[] args) {

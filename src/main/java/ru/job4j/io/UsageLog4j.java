705238307a9d4a13b4550749b6c11e84d151cbec
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-09-22 20:16:35 +0300

    3. Slf4j - вывод переменных. [#268852 #15006]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,15 +1,24 @@
 package ru.job4j.io;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 public class UsageLog4j {
     
     private static final  Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace massage");
-        LOG.debug("Debug massage");
-        LOG.info("Info massage");
-        LOG.warn("warn massage");
-        LOG.error("Error massage");
+
+       String name = "Mikhail Pushkarev";
+       int age = 25;
+       boolean flag = false;
+       double weight = 69.9;
+       float vision = 2.00001f;
+       byte size = 39;
+       char ch = 'K';
+       long lg = 1000000000;
+       LOG.debug(
+               "User info name:  {}, age : {}, flag : {}, weight : {}, "
+                       + " vision : {}, size : {}, ch : {}, lg : {} ",
+               name, age, flag, weight, vision, size, ch, lg);
     }
 }
